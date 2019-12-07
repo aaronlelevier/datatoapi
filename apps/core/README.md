a6ea8040ecfd1a92458ababbb2a8fa3e1309c4cd
aaronlelevier <aaron.lelevier@gmail.com>
2019-12-07 10:31:11 -0800

    'core' app - first commit

---
+++ apps/core/README.md
@@ -0,0 +1,9 @@
+core
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ apps/core/src/core_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc core public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(core_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    core_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
