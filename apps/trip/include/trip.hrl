ec59974e9e9c5d4019094454ead8059c20399f99
aaronlelevier <aaron.lelevier@gmail.com>
2019-12-14 06:35:00 -0800

    add 'user' record

--- apps/trip/include/trip.hrl
+++ apps/trip/include/trip.hrl
@@ -7,11 +7,17 @@
 %%% Created : 13. Dec 2019 6:34 AM
 %%%-------------------------------------------------------------------
 -author("aaron lelevier").
--export([]).
 
-%% @doc a single trip consisting of multiple trip points
+%% @doc User that makes Trips
+-record(user, {
+  id = 0,
+  username = ""
+}).
+
+%% @doc a single Trip consisting of multiple TripPoint's
 -record(trip, {
   id = 0,
+  user_id = #user.id,
   trip_points = [],
   name = "",
   start = {0.0, 0.0},
@@ -19,11 +25,11 @@
   distance = 0.0
 }).
 
-%% @doc a single point along a trip
+%% @doc a single point along a Trip
 -record(trip_point, {
   id = 0,
   trip_id = #trip.id,
-  dt={{2019, 12, 13}, {6, 42, 0}},
-  long=0.0,
-  lat=0.0
+  dt = {{2019, 12, 13}, {6, 42, 0}},
+  long = 0.0,
+  lat = 0.0
 }).
