51ae3b8144fc3f057da83924671fa9ff35de59ac
aaronlelevier <aaron.lelevier@gmail.com>
2019-12-18 06:18:29 -0800

    common test - example from docs

---
+++ test/trip/trip_SUITE.erl
@@ -0,0 +1,18 @@
+%% @doc learning `common test`
+%% docs: https://erlang.org/doc/apps/common_test/getting_started_chapter.html
+%%
+%% run all common test's cmd:
+%% $ rebar3 ct
+%%
+%% show common test CL args cmd:
+%% $ rebar3 help ct
+%% @end
+
+-module(trip_SUITE).
+-compile(export_all).
+
+all() ->
+  [mod_exists].
+
+mod_exists(_) ->
+  {module, trip} = code:load_file(trip).
