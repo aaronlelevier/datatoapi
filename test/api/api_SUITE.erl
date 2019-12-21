f3cb1791e6153f6dc48aa5aa7e12920f325e61bb
aaronlelevier <aaron.lelevier@gmail.com>
2019-12-21 07:30:14 -0800

    dry up api_SUITE common tests

--- test/api/api_SUITE.erl
+++ test/api/api_SUITE.erl
@@ -5,6 +5,8 @@
 -compile(export_all).
 -compile(nowarn_export_all).
 
+%% ct --------------------------------------------------------------------
+
 init_per_suite(Config0) ->
   {ok, _} = application:ensure_all_started(api),
   Config = [{base_url, "http://localhost:8080"} | Config0],
@@ -18,18 +20,19 @@ all() ->
 
 %% tests --------------------------------------------------------------------
 
-can_get_index_endpoint(Config) ->
+can_fetch_index(Config) ->
   Url = build_url(Config, "/"),
   {ok, _Response} = httpc:request(Url),
   ok.
 
-can_get_user_list_endpoint(Config) ->
-  Url = build_url(Config, "/user"),
-  {ok, Response} = httpc:request(Url),
-  Bin = api_util:response_to_binary(Response),
-  L = jsx:decode(Bin),
-  true = is_list(L),
-  true = length(L) > 0.
+can_fetch_user_list(Config) ->
+  do_fetch_returns_data(Config, "/user").
+
+can_fetch_user_with_id(Config) ->
+  do_fetch_returns_data(Config, "/user/1").
+
+can_get_user_with_query_param(Config) ->
+  do_fetch_returns_data(Config, "/user/?username=aaron").
 
 %% helpers --------------------------------------------------------------------
 
@@ -38,3 +41,11 @@ can_get_user_list_endpoint(Config) ->
 build_url(Config, Segment) ->
   BaseUrl = proplists:get_value(base_url, Config),
   BaseUrl ++ Segment.
+
+do_fetch_returns_data(Config, UrlSegment) ->
+  Url = build_url(Config, UrlSegment),
+  {ok, Response} = httpc:request(Url),
+  Bin = api_util:response_to_binary(Response),
+  L = jsx:decode(Bin),
+  true = is_list(L),
+  true = length(L) > 0.
